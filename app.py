0d52c7216c44d715420e861275ab9bd34f543b23
Gautham Mahadevan <128295307+gauthamgtg@users.noreply.github.com>
2025-10-11 16:45:40 +0530

    Add daily ad statistics query and update DataFrame display for yesterday's disapproved ads
    - Introduced a new SQL query to fetch ad statistics specifically for the previous day, including metrics such as total ads and disapproved ads.
    - Updated the DataFrame display to present daily disapproved ads statistics alongside overall metrics for better clarity and insights.
    - Enhanced percentage calculations for disapproved ads to ensure accurate representation in the display.

--- app.py
+++ app.py
@@ -211,6 +211,138 @@ order by ad_account_id
 group by 1,2,3,4,5,6,7,8
     '''
 
+yesterday_query = '''
+SELECT buid,bu.name as business_name,bu.email as email,a.ad_account_id,b.currency,
+case when flag='Others' then 'Active' else COALESCE(flag,'Active') end as status,disable_date,disable_reason,
+sum(case when ad_status = 'APPROVED' then 1 else 0 end) as total_ads,
+sum(case when ad_status = 'DISAPPROVED' then 1 else 0 end) as disapproved_ads,
+sum(sevend_spends) as "7d_spends",
+sum(current_month_spends) as current_month_spends,
+sum(thirtyd_spends) as "30d_spends",
+sum(lifetime_spends) as lifetime_spends
+from
+(
+SELECT a.ad_account_id,a.ad_id,ad_status,effective_status,status_date,ad_review_feedback
+ FROM
+(
+SELECT a.ad_account_id,ad_id,ad_status,effective_status,status_date,ad_review_feedback
+ FROM
+(
+select fad.ad_account_id,ad_id, 
+case when effective_status ='DISAPPROVED' then 'DISAPPROVED' else 'APPROVED' end as ad_status,effective_status,
+ date(fad.updated_at) as status_date,
+row_number() over(PARTITION by ad_id order by date(fad.updated_at) desc) as rw,ad_review_feedback
+from zocket_global.fb_ads_details_v3 fad
+join zocket_global.fb_child_ad_accounts fcaa on fad.ad_account_id = fcaa.ad_account_id
+where date(fad.updated_at)=current_date-1 and effective_status='DISAPPROVED'
+)a
+where rw=1
+) a
+) a
+left JOIN
+(select ad_account_id,
+SUM(CASE WHEN date(date_start) > CURRENT_DATE - INTERVAL '7 DAY' THEN spend::float ELSE 0 END) AS "sevend_spends",
+SUM(CASE WHEN date(date_start) > CURRENT_DATE - INTERVAL '30 DAY' THEN spend::float ELSE 0 END) AS "thirtyd_spends",
+SUM( CASE WHEN date_start >= DATE_TRUNC('month', CURRENT_DATE) THEN spend::float ELSE 0 END) AS current_month_spends,
+sum(spend) as lifetime_spends from zocket_global.ad_account_spends aas
+group by 1
+order by 4 desc
+)s on a.ad_account_id = s.ad_account_id
+left join (
+    select ad_account_id,buid,created_at,prev_date,currency
+from
+(
+    select faa.ad_account_id,app_business_id,buid,faa.created_at,currency,
+    row_number() over(PARTITION by faa.ad_account_id order by date(faa.created_at) desc) as rw,
+     coalesce(date(lag(faa.created_at,1) over(partition by faa.ad_account_id order by date(faa.created_at) desc)),date('2099-12-31')) as prev_date
+
+     from zocket_global.fb_child_ad_accounts faa
+     left join zocket_global.fb_child_business_managers fcbm on faa.app_business_manager_id=fcbm.id
+left join 
+    (SELECT
+    id ,name,brand_name,json_extract_path_text(json_extract_array_element_text(business_user_ids, 0), 'role') AS role,
+    json_extract_path_text(json_extract_array_element_text(business_user_ids, 0), 'business_user_id') AS buid
+FROM
+    zocket_global.business_profile
+WHERE
+    json_extract_path_text(json_extract_array_element_text(business_user_ids, 0), 'role') = 'owner' )bp on fcbm.app_business_id=bp.id
+)
+where rw=1
+)b on 
+a.ad_account_id =b.ad_account_id
+left join zocket_global.business_users bu on b.buid=bu.id
+left JOIN (
+    select ad_account_id,disable_date,flag,disable_reason
+    FROM
+    (
+    SELECT ad_account_id,disable_date,flag,disable_reason,row_number() over(PARTITION by ad_account_id order by coalesce(date(disable_date),date('2000-01-01') )desc ) as rw
+    FROM
+    (
+    SELECT euid,ad_account_id,
+case when flag = 'Reactivated' then reactivation_date
+when flag = 'Disabled' then dt end as disable_date,
+case when flag = 'Reactivated' then dt end as reactivation_date
+,flag
+,currency
+,name as ad_account_name,bm_name,
+case when disable_reason = 0 then 'NONE'
+when disable_reason = 1 then  'ADS_INTEGRITY_POLICY'
+when disable_reason = 2 then  'ADS_IP_REVIEW'
+when disable_reason = 3 then  'RISK_PAYMENT'
+when disable_reason = 4 then  'GRAY_ACCOUNT_SHUT_DOWN'
+when disable_reason = 5 then  'ADS_AFC_REVIEW'
+when disable_reason = 6 then  'BUSINESS_INTEGRITY_RAR'
+when disable_reason = 7 then  'PERMANENT_CLOSE'
+when disable_reason = 8 then  'UNUSED_RESELLER_ACCOUNT'
+when disable_reason = 9 then  'UNUSED_ACCOUNT'
+when disable_reason = 10 then  'UMBRELLA_AD_ACCOUNT'
+when disable_reason = 11 then  'BUSINESS_MANAGER_INTEGRITY_POLICY'
+when disable_reason = 12 then  'MISREPRESENTED_AD_ACCOUNT'
+when disable_reason = 13 then  'AOAB_DESHARE_LEGAL_ENTITY'
+when disable_reason = 14 then  'CTX_THREAD_REVIEW'
+when disable_reason = 15 then  'COMPROMISED_AD_ACCOUNT' end as disable_reason
+FROM
+(
+SELECT *,case when rw = 1 and prev_status !=1 and account_status = 1 then 'Reactivated' 
+            when rw = 1 and account_status != 1 then 'Disabled' else 'Others'
+            end as flag, case when rw = 1 and prev_status !=1 and account_status = 1 then prev_dt end as reactivation_date
+
+FROM
+(
+select coalesce(eu.euid,cast(bp.buid as int)) as euid,COALESCE(b.name,d.name)as name,a.ad_account_id,a.account_status,disable_reason,dateadd('minute',330,a.created_at) as dt,
+COALESCE(b.currency,d.currency)as currency,
+COALESCE(c.name,e.name)as bm_name,
+ row_number() over(partition by a.ad_account_id order by a.created_at desc) as rw,
+ lag(a.account_status,1) over(PARTITION by a.ad_account_id order by dateadd('minute',330,a.created_at)) as prev_status,
+ lag(dateadd('minute',330,a.created_at),1) over(PARTITION by a.ad_account_id order by dateadd('minute',330,a.created_at)) as prev_dt
+-- from "dev"."public"."ad_account_webhook" a
+from "dev"."z_b"."ad_account_webhook" a
+left join fb_ad_accounts b on a.ad_account_id = b.ad_account_id
+left join fb_business_managers c on c.id = b.app_business_manager_id
+left join zocket_global.fb_child_ad_accounts d on a.ad_account_id = d.ad_account_id
+left join zocket_global.fb_child_business_managers e on e.id = d.app_business_manager_id
+left join 
+    (SELECT
+    id ,name,brand_name,json_extract_path_text(json_extract_array_element_text(business_user_ids, 0), 'role') AS role,
+    json_extract_path_text(json_extract_array_element_text(business_user_ids, 0), 'business_user_id') AS buid
+FROM
+    zocket_global.business_profile
+WHERE
+    json_extract_path_text(json_extract_array_element_text(business_user_ids, 0), 'role') = 'owner' )bp on e.app_business_id=bp.id
+left join enterprise_users eu on c.app_business_id=eu.euid
+-- where a.ad_account_id='act_635291785907397'
+order by 3
+)
+-- where ad_account_id='act_635291785907397'
+order by ad_account_id
+)
+    )
+    )
+    where rw=1 
+)da on a.ad_account_id=da.ad_account_id
+-- where a.ad_account_id='act_1808607866379064'
+group by 1,2,3,4,5,6,7,8
+'''
 
 @st.cache_data(ttl=36400)  # 86400 seconds = 24 hours
 @redshift_connection(db,name,passw,server,port)
@@ -224,7 +356,7 @@ def execute_query(connection, cursor,query):
 
 # df = execute_query(query=query)
 df = execute_query(query=query)
-
+df_yesterday = execute_query(query=yesterday_query)
 
 df['Disapproved_Percentage'] = df['disapproved_ads'] / df['total_ads']
 df['Disapproved_Percentage'] = df['Disapproved_Percentage'].fillna(0)
@@ -236,9 +368,21 @@ df['7d_Disapproved_Percentage'] = df['7d_Disapproved_Percentage'].fillna(0)
 df['7d_Disapproved_Percentage'] = df['7d_Disapproved_Percentage'].apply(lambda x: "0%" if pd.isna(x) or np.isinf(x) or x == 0 else f"{round(x*100):.0f}%")
 
 
+df_yesterday['Disapproved_Percentage'] = df_yesterday['disapproved_ads'] / df_yesterday['total_ads']
+df_yesterday['Disapproved_Percentage'] = df_yesterday['Disapproved_Percentage'].fillna(0)
+df_yesterday['Disapproved_Percentage'] = df_yesterday['Disapproved_Percentage'].apply(lambda x: "0%" if pd.isna(x) or np.isinf(x) or x == 0 else f"{round(x*100):.0f}%")
+
 st.title("Disapproved Ads Stats")
 
-# st.dataframe(df[['buid','business_name','email','ad_account_id','status','currency','disable_date','disable_reason','total_ads','disapproved_ads','Disapproved_Percentage','total_ads_last7days','disapproved_ads_last7days','7d_Disapproved_Percentage','7d_spends','current_month_spends','30d_spends','lifetime_spends']],use_container_width=True)
+
+st.title("Daily Disapproved Ads Stats")
 
 
-st.dataframe(df[['buid','business_name','email','ad_account_id','status','currency','disable_date','disable_reason','total_ads','disapproved_ads','Disapproved_Percentage','total_ads_last7days','disapproved_ads_last7days','7d_Disapproved_Percentage']],use_container_width=True)
+st.dataframe(df_yesterday[['buid','business_name','email','ad_account_id','currency','status','disable_date','disable_reason','total_ads','disapproved_ads','Disapproved_Percentage']],use_container_width=True)
+
+
+
+st.title("Overall Disapproved Ads Stats")
+
+st.dataframe(df[['buid','business_name','email','ad_account_id','currency','status','disable_date','disable_reason','total_ads','disapproved_ads','Disapproved_Percentage','total_ads_last7days','disapproved_ads_last7days','7d_Disapproved_Percentage']],use_container_width=True)
+
